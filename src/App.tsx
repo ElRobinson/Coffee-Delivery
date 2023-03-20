5d2eda798136ee73443cf9de5aa136dfb62f5056
Luis Felipe Trein Robinson <luisfelipe.robinson@gmail.com>
2023-03-20 19:22:22 -0300

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
-import { useState } from 'react'
 
 function App() {  
 
